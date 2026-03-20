4c6a17d7855b209677bf68ea4b20a65647f8e5c4
Ptriantafyll <ptriantafyll@gmail.com>
2026-03-21 00:28:35 +0200

    Added model variables

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,7 @@
-from models import Department
+from models import Department, Doctor
 import datetime
 import calendar
+from ortools.sat.python import cp_model
 
 
 class ScheduleApp:
@@ -20,13 +21,30 @@ class ScheduleApp:
         is_weekend = {d: (d.weekday() >= 5) for d in dates}
         return dates, is_weekend
 
+    def _build_model(self, dates):
+        """Creates the CP-SAT model and shift assignment variables."""
+        self.model = cp_model.CpModel()
+
+        self.shift_assignments = {}
+
+        for department in self.departments:
+            for position in department.positions:
+                for shift in position.shifts:
+                    for doctor in department.doctors:
+                        for day_index, date in enumerate(dates):
+                            if date in doctor.unavailability:
+                                continue
+
+                            self.shift_assignments[(day_index, shift, doctor)] = self.model.NewBoolVar(
+                                f"shift_assignment_{day_index}_{position}_{shift.name}_{doctor}")
+
     def create_schedule(self, month: int, year: int):
         pass
 
 
 if __name__ == "__main__":
     app = ScheduleApp(departments=[])
-    dates, is_weekend = app._calculate_days_for_schedule(month=5, year=2026)
+    dates, is_weekend = app._calculate_days_for_schedule(month=4, year=2026)
     print(dates[0], "→", dates[-1])
     print("Days in month:", len(dates))
     print("Weekend days:", sum(1 for d in dates if is_weekend[d]))
