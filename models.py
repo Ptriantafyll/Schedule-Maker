f6d1e5d5c3791d6c92f0528f4614d8ff8331f4a7
Ptriantafyll <ptriantafyll@gmail.com>
2026-04-04 18:03:58 +0300

    Refactoring ,pylint/ruff/vulture/radon checking

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
+"""
+Defines data models for the scheduling application. 
+Doctors, shifts, positions, teams, and departments.
+"""
+
 from dataclasses import dataclass, field
 import datetime
 from typing import Optional
 
 
 @dataclass(eq=False)
-class ScheduleConfig:
+class ScheduleConfig:  # pylint: disable=too-many-instance-attributes
+    """ Configuration for scheduling constraints and solver settings."""
+
     # Soft constraint weights
     w_every_other_penalty: int = 4
     w_gap_penalty: int = 2
@@ -18,11 +25,12 @@ class ScheduleConfig:
     max_duties_per_month: int = 8
 
     # Schedule variables
-    month_blocks = 3
+    month_blocks: int = 3
 
 
 @dataclass(eq=False)
 class Shift:
+    """ Represents a work shift, such as "Morning", "Afternoon", or "Night". """
     name: str
     doctors_per_shift: int = 1
     grants_day_off: bool = False
@@ -30,6 +38,7 @@ class Shift:
 
 @dataclass(eq=False)
 class Doctor:
+    """ Represents a doctor working in the department. """
     name: str
     email: str
     unavailability: set[datetime.date] = field(default_factory=set)
@@ -39,6 +48,7 @@ class Doctor:
 
 @dataclass(eq=False)
 class Position:
+    """ Represents a position that needs to be staffed, such as "ER" or "ICU". """
     name: str
     shifts: list[Shift] = field(default_factory=list)
     duty_days: set[int] = field(default_factory=lambda: {0, 1, 2, 3, 4, 5, 6})
@@ -47,21 +57,29 @@ class Position:
 
 @dataclass(eq=False)
 class Team:
+    """ Represents a team of doctors working together. """
     name: str
     doctors: list[Doctor] = field(default_factory=list)
 
 
 @dataclass(eq=False)
 class Department:
+    """ Represents a hospital department with its scheduling needs. """
     name: str
     positions: list[Position] = field(default_factory=list)
     teams: list[Team] = field(default_factory=list)
     config: ScheduleConfig = field(default_factory=ScheduleConfig)
     backup_department: Optional['Department'] = None
     teamless_doctors: list[Doctor] = field(default_factory=list)
+    # to remove later
+    doctor_order: list[Doctor] = field(default_factory=list)
 
     @property
     def doctors(self) -> list[Doctor]:
+        """ 
+        Returns a list of all doctors in the department
+        Including those in teams and teamless doctors.
+        """
         team_doctors = [
             doctor for team in self.teams for doctor in team.doctors]
         return team_doctors + self.teamless_doctors
