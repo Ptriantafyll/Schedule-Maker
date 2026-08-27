f2c5935304db8a2b92c49250913ebea4aa3b0697
Ptriantafyll <ptriantafyll@gmail.com>
2026-08-27 17:06:04 +0300

    Added missing claims test

--- backend/src/auth/tests/test_security.py
+++ backend/src/auth/tests/test_security.py
@@ -49,10 +49,42 @@ def test_access_token_security_claims_cannot_be_overridden():
 def test_decode_access_token_rejects_expired_token():
     """Tests that an expired token is rejected"""
     sub = str(uuid.uuid4())
-    access_token = security.create_access_token({
-        "sub": sub},
+    access_token = security.create_access_token(
+        {"sub": sub},
         expires_delta=datetime.timedelta(seconds=-1)
     )
 
     with pytest.raises(jwt.ExpiredSignatureError):
         security.decode_access_token(access_token)
+
+
+@pytest.mark.parametrize(
+    "missing_claim",
+    [
+        "sub",
+        "exp",
+        "iat",
+        "jti",
+        "iss",
+        "aud",
+        "token_type",
+    ]
+)
+def test_decode_access_token_rejects_missing_sub(missing_claim):
+    """Tests missing sub"""
+    access_token = security.create_access_token(
+        {"sub": str(uuid.uuid4())}
+    )
+    payload = security.decode_access_token(access_token)
+    payload.pop(missing_claim)
+
+    token_missing_claim = jwt.encode(
+        payload,
+        security.SECRET_KEY,
+        algorithm=security.ALGORITHM
+    )
+
+    with pytest.raises(jwt.MissingRequiredClaimError) as exc_info:
+        security.decode_access_token(access_token)
+
+    assert exc_info.value.claim == missing_claim
