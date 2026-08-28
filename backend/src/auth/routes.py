5a2904597952c5baba1f92136a79fdadc9d17e3f
Ptriantafyll <ptriantafyll@gmail.com>
2026-08-28 11:13:44 +0300

    Added route file

--- backend/src/auth/routes.py
+++ backend/src/auth/routes.py
@@ -3,10 +3,14 @@ Authentication routes
 """
 
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session
 
+from src.auth import controllers as auth_controllers
 from src.auth.schemas import Token
 from src.auth.dependencies import get_current_user
 from src.user.models import User as UserModel
+from src.db.connection import get_session
 
 router = APIRouter(
     prefix="/auth",
@@ -18,3 +22,12 @@ router = APIRouter(
 def get_current_user_profile(current_user: UserModel = Depends(get_current_user)):
     """Returns the profile of the currently authenticated user."""
     return current_user
+
+
+@router.post("/login", response_model=Token)
+def user_login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session)
+):
+    """Returns the token for the user that logs in"""
+    return auth_controllers.login_controller(form_data.username, form_data.password, session)
