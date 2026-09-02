b0942ab095175713c030e452a8b94413dff7a5bb
Ptriantafyll <ptriantafyll@gmail.com>
2026-09-02 10:08:17 +0300

    Added require department scope in dependencies

--- backend/src/auth/dependencies.py
+++ backend/src/auth/dependencies.py
@@ -51,6 +51,17 @@ def get_current_user(
     return user
 
 
+def require_department_scope(current_user: UserModel) -> uuid.UUID:
+    """"""
+    if current_user.department_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid account scope."
+        )
+
+    return current_user.department_id
+
+
 def require_role(*allowed_roles: UserRole):
     """Factory dependent for role enforcement"""
     def role_guard(current_user: UserModel = Depends(get_current_user)) -> UserModel:
